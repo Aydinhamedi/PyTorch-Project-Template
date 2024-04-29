7f38ea5b32ba345242897cafc8425ad430d6501f
Aydin <108932477+Aydinhamedi@users.noreply.github.com>
2024-04-29 17:21:58 +0330

    modified:   Utils/adabound.py

--- Utils/adabound.py
+++ Utils/adabound.py
@@ -105,7 +105,7 @@ class AdaBound(Optimizer):
                 state["step"] += 1
 
                 if group["weight_decay"] != 0:
-                    grad = grad.add(group["weight_decay"], p.data)
+                    grad = grad.add(p.data, alpha=group['weight_decay'])
 
                 # Decay the first and second moment running average coefficient
                 exp_avg.mul_(beta1).add_(1 - beta1, grad)
